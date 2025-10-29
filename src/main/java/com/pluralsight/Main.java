1229ebf6abb0a32fad6d2f9bac401979d4657de5
ayahrajab818-code <ayahrajab818@gmail.comgit config --global user.email ayahrajab818@gmail.comgit config --global user.email ayahrajab818@gmail.com>
2025-10-29 15:42:18 -0400

    done

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -13,11 +13,14 @@ public class Main {
         System.out.println("------------------------------");
 
         Vehicle car = new Vehicle("Personal Car", "2023-06-10", 30000,
-                "Nissa Sentra", 2023, 120000);
+                "Nissa Sentra", 2023, 1200);
+        System.out.println("------------------------------");
+
+        Cash cash = new Cash("phone", "2025-09-10", 1200);
 
         assets.add(house);
         assets.add(car);
-
+        assets.add(cash);
 
         for (Asset asset : assets) {
             System.out.println("Description: " + asset.getDescription());
@@ -37,6 +40,9 @@ public class Main {
                 System.out.println("Make & Model: " + car.getMakeModel());
                 System.out.println("Year: " + car.getYear());
                 System.out.println("Odometer:" + car.getOdometer());
+            }
+            else if (asset instanceof Cash) {
+                System.out.println("here is your cash");
             }
 
             System.out.println("-----------------------------");
